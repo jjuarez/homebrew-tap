159ee16d729844751c4bd5f701d947c597dc9818
Javier Juarez <javier.juarez@clarity.ai>
2020-05-10 13:57:41 +0200

    Avoids the installation from source using cargo, just trying to intall the binary

--- kubie.rb
+++ kubie.rb
@@ -5,9 +5,7 @@ class Kubei < Formula
   version  "0.9.1"
   sha256   "c789bcc4ecd02e1abd4e82d2df5f6529815cfeea84ee90b66437c8671a015d5f"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    bin.install "kubie"
   end
 end
