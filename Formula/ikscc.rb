98a86c20588ae4eb484ffb366b005c90413f12f6
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-02-26 14:01:56 +0100

    Extremely simple formula

--- Formula/ikscc.rb
+++ Formula/ikscc.rb
@@ -4,12 +4,5 @@ class Ikscc < Formula
   version  "0.3.0"
   url      "https://github.com/jjuarez/iks-ctx-cleaner/releases/download/v#{version}/iks-ctx-cleaner_#{version}_Darwin_x86_64.tar.gz"
   sha256   "7e0b4aae4fbf6a16a34ee54a348e560fdf5c284244982e911e1dca726dbac49b"
-
-  def install
-    system "install iks-ctx-cleaner /usr/local/bin/ikscc"
-  end
-
-  test do
-    system "true"
-  end
+  bin.install "iks-ctx-cleaner" => "ikscc"
 end
